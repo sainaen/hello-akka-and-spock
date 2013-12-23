8e1acfebfa2699844092d7246b342c126469b237
sainaen <sainaen@gmail.com>
2013-12-23 14:50:05 +0200

    Add simple Greeter actor

--- src/main/java/org/sainaen/HelloAkkaJava.java
+++ src/main/java/org/sainaen/HelloAkkaJava.java
@@ -1,9 +1,26 @@
 package org.sainaen;
 
+import akka.actor.UntypedActor;
+
 import java.io.Serializable;
 
 public class HelloAkkaJava {
     public static void main(String[] args) {
+    }
+
+    public static class Greeter extends UntypedActor {
+        String greeting = "";
+
+        @Override
+        public void onReceive(Object message) throws Exception {
+            if (message instanceof Whom) {
+                greeting = "Hello, " + ((Whom) message).whom + "!";
+            } else if (message instanceof Greet) {
+                getSender().tell(new Greeting(greeting), getSelf());
+            } else {
+                unhandled(message);
+            }
+        }
     }
 
     public static class Greet implements Serializable {}
