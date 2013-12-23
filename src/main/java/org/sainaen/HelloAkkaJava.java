bb2b2eeb5b794a2701a079864fea93a4d8fea5eb
sainaen <sainaen@gmail.com>
2013-12-23 14:46:20 +0200

    Add Akka messages classes: Greet, Whom, Greeting

--- src/main/java/org/sainaen/HelloAkkaJava.java
+++ src/main/java/org/sainaen/HelloAkkaJava.java
@@ -1,6 +1,24 @@
 package org.sainaen;
 
+import java.io.Serializable;
+
 public class HelloAkkaJava {
     public static void main(String[] args) {
+    }
+
+    public static class Greet implements Serializable {}
+
+    public static class Whom implements Serializable {
+        public final String whom;
+        public Whom(String whom) {
+            this.whom = whom;
+        }
+    }
+
+    public static class Greeting implements Serializable {
+        public final String message;
+        public Greeting(String message) {
+            this.message = message;
+        }
     }
 }
